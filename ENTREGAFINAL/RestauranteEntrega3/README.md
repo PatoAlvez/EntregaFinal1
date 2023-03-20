ea42510d218c414068fb45f1b14c145b90116541
PatoAlvez <124320602+PatoAlvez@users.noreply.github.com>
2023-03-20 19:19:16 -0300

    Delete ENTREGAFINAL directory

--- ENTREGAFINAL/RestauranteEntrega3/README.md
+++
@@ -1,1 +0,0 @@
-# RestauranteEntrega3
--- ENTREGAFINAL/restaurante/restaurant/MiRestaurante/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from MiRestaurante.models import Entrada, PlatoPrincipal, Postre, Avatar
-
-# Register your models here.
-
-admin.site.register(Entrada)
-admin.site.register(PlatoPrincipal)
-admin.site.register(Postre)
-admin.site.register(Avatar)
